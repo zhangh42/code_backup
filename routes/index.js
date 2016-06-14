ed0945aa10865b27c181a183ca9d93a377cf22ef
zhh <617146873@qq.com>
2016-06-14 19:11:56 +0800

    备份

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,30 @@ router.get('/register', function (req, res, next) {
     res.render('register')
 });
 
+router.get('/home', function (req, res, next) {
+
+    res.render('home', {})
+});
+
+router.get('/read', function (req, res, next) {
+
+    res.render('read', {})
+});
+
+router.get('/receive', function (req, res, next) {
+
+    res.render('receive', {})
+});
+
+router.get('/status', function (req, res, next) {
+
+    res.render('status', {})
+});
+
+router.get('/write', function (req, res, next) {
+
+    res.render('write', {})
+});
 
 // 处理post请求
 router.post('/admin_form', function (req, res, next) {
@@ -23,8 +47,9 @@ router.post('/admin_form', function (req, res, next) {
 });
 
 router.post('/register_form', function (req, res, next) {
-    var body = util.inspect((req.body));
-    res.end(body);
-})
+    var body =req.body;
+    console.log(typeof (body));
+    res.render('home',body);
+});
 
 module.exports = router;
